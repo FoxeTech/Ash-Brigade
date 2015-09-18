faec389b04ba6d67d59a6831914d90cbc680e3d8
Chandler-Thompson <iamdrachir@gmail.com>
2015-09-18 16:18:02 -0400

    Added common code
    Added TextFileManipulator, TextManipulation, and
    BasicLineOfSightDetection

---
+++ Ash Brigade/src/BasicLineOfSightDetection.java
@@ -0,0 +1,75 @@
+package common;
+
+import java.util.ArrayList;
+import controller.FieldController;
+import model.Constants;
+
+public class BasicLineOfSightDetection {
+
+	Constants constants;
+	FieldController fieldController;
+	
+	//dimensions of playing field
+	int width;
+	int length;
+	int height;
+	
+	public BasicLineOfSightDetection(int width, int length, int height) {
+		
+		constants = new Constants();
+		fieldController = new FieldController();
+		
+		this.width = width;
+		this.length = length;
+		this.height = height;
+		
+	}
+	
+	public ArrayList<ArrayList<Integer>> getLOS(int x, int y, int z, int distance){
+		ArrayList<ArrayList<Integer>> losList = new ArrayList<>();
+		
+		for(int i = 0; i < constants.NUM_DIMENSIONS; i++){//one list for each dimension
+			losList.add(new ArrayList<Integer>());
+		}
+		
+		return calculateLOS(x, y, z, 0, distance, losList);
+		
+	}
+	
+	private ArrayList<ArrayList<Integer>> calculateLOS(int x, int y, int z, int step, int distance, ArrayList<ArrayList<Integer>> losList){
+		if(step == distance || fieldController.isObstacle(x, y, z))
+			return losList;
+		else{
+			
+			if(!isDuplicate(losList, x, y, z)){
+				losList.get(0).add(x);
+				losList.get(1).add(y);
+				losList.get(2).add(z);
+			}
+				
+			if(x < width)
+				calculateLOS(x+1, y, z, step+1, distance, losList);
+			if(x >= 0)
+				calculateLOS(x-1, y, z, step+1, distance, losList);
+			if(y < length)
+				calculateLOS(x, y+1, z, step+1, distance, losList);
+			if(y >= 0)
+				calculateLOS(x, y-1, z, step+1, distance, losList);
+			if(z < height)
+				calculateLOS(x, y, z+1, step+1, distance, losList);
+			if(z >= 0)
+				calculateLOS(x, y, z-1, step+1, distance, losList);
+			
+		}
+		return losList;
+	}
+	
+	private boolean isDuplicate(ArrayList<ArrayList<Integer>> losList, int x, int y, int z){
+		for(int i = 0; i < losList.size(); i++){
+			if(losList.get(0).get(i) == x && losList.get(1).get(i) == y && losList.get(2).get(i) == z)
+				return true;
+		}
+		return false;
+	}
+
+}
---
+++ Ash Brigade/src/Graphics/Textures.java
@@ -0,0 +1,31 @@
+package Graphics;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
+
+public class Textures {
+	
+	//create Texture variables here
+	public static Texture TestTexture;//Example variable
+	
+	public Textures(){
+		//initialize them here by using function below
+		TestTexture = loadTexture("test","png");//example initialization
+	}
+	
+	
+	
+	//first variable is filename, path begins in the res folder, the extension is the file extension
+	//The extension Variable needs to be lower case
+	public static Texture loadTexture(String key, String extension){
+		try {
+			return	TextureLoader.getTexture(extension, new FileInputStream(new File("res/" + key + "." + extension)));
+		} catch (IOException e) {
+			System.out.println("Error Loading Texture");
+		}
+		return null;
+	}
+}
