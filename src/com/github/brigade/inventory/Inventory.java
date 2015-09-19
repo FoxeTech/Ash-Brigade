3f2bb8a45cfa9d34fa261e2365979bc30c23b229
Matt <lordpankake@gmail.com>
2015-09-19 00:20:00 -0400

    Inventory sectionalization
    Inventory is now handled in sections (Armor, Potion, Weapons, Misc)
    Items now can return their position in the inventory

--- src/com/github/brigade/inventory/Inventory.java
+++ src/com/github/brigade/inventory/Inventory.java
@@ -2,10 +2,15 @@ package com.github.brigade.inventory;
 
 import com.github.brigade.exception.InventoryException;
 
+/**
+ * <b>TODO:</b> Make method for increasing/decreasing the inventory
+ *
+ */
 public class Inventory {
-	// TODO: Have sectionalized items (Combat items, Potions, etc.)
-	// Could be a function to just get a certain range or have separate arrays.
-	private final Item[] items = new Item[20];
+	private final Item[] potions = new Item[16];
+	private final Item[] armor = new Item[6];
+	private final Item[] weapons = new Item[6];
+	private final Item[] misc = new Item[6];
 
 	/**
 	 * Sets the inventory's item array to another item array. <br>
@@ -17,7 +22,7 @@ public class Inventory {
 	 * Item[] newInventory = new Item[invLength];
 	 * // Load items into the array
 	 * try {
-	 * 	inv.loadItems(newInventory);
+	 * 	inv.loadItems(newInventory, EnumItemType.Potion);
 	 * } catch (InventoryException e) {
 	 * 	// Handle the error.
 	 * }
@@ -25,65 +30,144 @@ public class Inventory {
 	 * 
 	 * @param items
 	 *            The new array of items to become the inventory
+	 * @param type
+	 *            The inventory type
 	 * @throws InventoryException
 	 *             If the size of the parameter items does not match the
 	 *             inventory's length
 	 */
-	public void loadItems(Item[] items) throws InventoryException {
-		if (this.items.length != items.length) {
-			throw new InventoryException("Tried loading a inventory of invalid size (Loaded:" + items.length + ", Expected:" + this.items.length + ")");
-		}
-		for (int i = 0; i < items.length; i++) {
-			setItem(i, items[i]);
+	public void loadItems(Item[] items, EnumItemType type) throws InventoryException {
+		switch (type) {
+		case Armor:
+			if (this.armor.length != items.length) {
+				throw new InventoryException("Tried loading a inventory of invalid size (Loaded:" + items.length + ", Expected:" + this.armor.length + ")");
+			}
+			for (int i = 0; i < items.length; i++) {
+				setItem(i, items[i], type);
+			}
+			break;
+
+		case Potion:
+			if (this.potions.length != items.length) {
+				throw new InventoryException("Tried loading a inventory of invalid size (Loaded:" + items.length + ", Expected:" + this.potions.length + ")");
+			}
+			for (int i = 0; i < items.length; i++) {
+				setItem(i, items[i], type);
+			}
+			break;
+		case Weapon:
+			if (this.weapons.length != items.length) {
+				throw new InventoryException("Tried loading a inventory of invalid size (Loaded:" + items.length + ", Expected:" + this.weapons.length + ")");
+			}
+			for (int i = 0; i < items.length; i++) {
+				setItem(i, items[i], type);
+			}
+			break;
+		case Misc:
+			if (this.misc.length != items.length) {
+				throw new InventoryException("Tried loading a inventory of invalid size (Loaded:" + items.length + ", Expected:" + this.misc.length + ")");
+			}
+			for (int i = 0; i < items.length; i++) {
+				setItem(i, items[i], type);
+			}
+			break;
+		default:
+			break;
 		}
 	}
 
 	/**
-	 * Sets an item in the inventory at a given index to a given item. <br>
-	 * <hr>
-	 * <b>TODO</b>: For ease of access, items can have a field (int invSlot)
-	 * that will be set here notifying it which slot it is in. The Item's field
-	 * will be handled here so that a dev could use code such as: <br>
+	 * Sets an item in the inventory at a given index (of the given type) to a
+	 * given item. <br>
 	 * 
+	 * Example:
 	 * <pre>
 	 * Item i = ... //get the item in some way
-	 * unit.getInventory().setItem(i.getInventorySlot(), new Item(...)); //Swap it out (potentially move it to another slot)
+	 * unit.getInventory().setItem(i.getInventorySlot(), new Item(...), EnumItemType.Potion);
 	 * </pre>
 	 * 
 	 * @param index
 	 *            Index for setting
 	 * @param item
 	 *            The new item
+	 * @param type
+	 *            The inventory type
+	 * @throws InventoryException Thrown if the index is over the intended array's bounds
 	 */
-	public void setItem(int index, Item item) {
-
-		this.items[index] = item;
+	public void setItem(int index, Item item, EnumItemType type) throws InventoryException {
+		switch (type) {
+		case Armor:
+			if (this.armor.length >= index) {
+				throw new InventoryException("Tried setting an item in an invalid index (Tried:" + index + ", Array Length:" + this.armor.length + ")");
+			}
+			this.armor[index] = item;
+			item.setInventorySlot(index);
+			break;
+		case Potion:
+			if (this.potions.length >= index) {
+				throw new InventoryException("Tried setting an item in an invalid index (Tried:" + index + ", Array Length:" + this.potions.length + ")");
+			}
+			this.potions[index] = item;
+			item.setInventorySlot(index);
+			break;
+		case Weapon:
+			if (this.weapons.length >= index) {
+				throw new InventoryException("Tried setting an item in an invalid index (Tried:" + index + ", Array Length:" + this.weapons.length + ")");
+			}
+			this.weapons[index] = item;
+			item.setInventorySlot(index);
+			break;
+		case Misc:
+			if (this.misc.length >= index) {
+				throw new InventoryException("Tried setting an item in an invalid index (Tried:" + index + ", Array Length:" + this.misc.length + ")");
+			}
+			this.misc[index] = item;
+			item.setInventorySlot(index);
+			break;
+		default:
+			break;
+		}
 	}
 
 	/**
-	 * Gets an item from the inventory at the given index.<br>
+	 * Gets an item from the inventory at the given index (of the given type).
+	 * <br>
 	 * Example usage:
 	 * 
 	 * <pre>
-	 * Item heldItem = unit.getInventory().getItem(0)); 
+	 * Item heldItem = unit.getInventory().getItem(0, EnumItemType.Potion)); 
 	 * if (heldItem != null){
 	 * //code Here
 	 * }
 	 * </pre>
 	 * 
 	 * @param index
-	 * @return
+	 * @param type
+	 *            The inventory type
+	 * @return Returns null if type is not valid
 	 */
-	public Item getItem(int index) {
-		return items[index];
+	public Item getItem(int index, EnumItemType type) {
+		switch (type) {
+		case Armor:
+			return this.armor[index];
+		case Misc:
+			return this.misc[index];
+		case Potion:
+			return this.potions[index];
+		case Weapon:
+			return this.weapons[index];
+		default:
+			break;
+		}
+		return null;
 	}
 
 	/**
-	 * Returns the array of items in the inventory. <br>
+	 * Returns the array of items of the given type in the inventory. <br>
 	 * Example usage:
 	 * 
 	 * <pre>
-	 * for (Item item : unit.getInventory().getItems()) {
+	 * for (Item item : unit.getInventory().getItems(EnumItemType.Potion)) {
 	 * 	// Item slots not filled will be null
 	 * 	if (item == null)
 	 * 		continue;
@@ -91,18 +175,44 @@ public class Inventory {
 	 * }
 	 * </pre>
 	 * 
-	 * @return
+	 * @return Returns null if type is not valid
 	 */
-	public Item[] getItems() {
-		return items;
+	public Item[] getItems(EnumItemType type) {
+		switch (type) {
+		case Armor:
+			return this.armor;
+		case Potion:
+			return this.potions;
+		case Weapon:
+			return this.weapons;
+		case Misc:
+			return this.misc;
+		default:
+			break;
+		}
+		return null;
 	}
 
 	/**
 	 * Returns the length of the inventory array.
 	 * 
-	 * @return items.length
+	 * @param type
+	 *            The inventory type
+	 * @return Returns -1 if type is not valid
 	 */
-	public int length() {
-		return items.length;
+	public int length(EnumItemType type) {
+		switch (type) {
+		case Armor:
+			return this.armor.length;
+		case Potion:
+			return this.potions.length;
+		case Weapon:
+			return this.weapons.length;
+		case Misc:
+			return this.misc.length;
+		default:
+			break;
+		}
+		return -1;
 	}
 }
--- src/com/github/brigade/inventory/Item.java
+++ src/com/github/brigade/inventory/Item.java
@@ -3,8 +3,8 @@ package com.github.brigade.inventory;
 public class Item {
 	private final String name;
 	private final int id;
-	// TODO: More reasons/uses to make the inventory sectionalized
 	private final EnumItemType itemType;
+	private int inventorySlot = -1;
 
 	/**
 	 * Instantiates the item with a name, id, and type.
@@ -34,12 +34,30 @@ public class Item {
 		return name;
 	}
 
+	/**
+	 * Updates the item, letting it know what slot in the inventory it is in.
+	 * 
+	 * @param inventorySlot
+	 *            The slot
+	 */
+	public void setInventorySlot(int inventorySlot) {
+		this.inventorySlot = inventorySlot;
+	}
+
+	/**
+	 * Get's the item's current index in the character's inventory.
+	 * 
+	 * @return Returns -1 if the slot has not been set
+	 */
+	public int getInventorySlot() {
+		return inventorySlot;
+	}
+
 	public int getId() {
 		return id;
 	}
 
 	public EnumItemType getItemType() {
-		// TODO:
 		return itemType;
 	}
 }
